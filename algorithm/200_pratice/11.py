a3fbb1ca937ee94113d584223c8728810fda7f2f
jinseong205 <jinseong205@gmail.com>
2020-08-23 20:15:29 +0900

    for coding test

---
+++ algorithm/200_pratice/11.py
@@ -0,0 +1,18 @@
+scope = [1,2,3,4,5,]
+for i in scope:
+    print(i, end = ' ')
+print()
+
+str = 'abcdef'
+for i in str:
+    print(i, end = ' ')
+print()
+
+ascii_codes = {'a':97, 'b':98, 'c':99}
+for i in ascii_codes:
+    print(i, end = ' ')
+print()
+
+for i in range(10):
+    print(i, end = ' ')
+print()
